79d6bcb1430499f751d854b984c54a488d373854
zzy <2948718416@qq.com>
2022-09-07 20:07:25 +0800

    666

---
+++ python字符串操作学习.py
@@ -0,0 +1,17 @@
+s='Hello World'
+print(s.upper())
+print(s.lower())
+print(s.swapcase())
+print(s.capitalize())
+print(s.title())
+s1='hello world'
+'''居中'''
+print(s1.center(20,'*'))
+'''左对齐'''
+print(s1.ljust(20,'*'))
+print(s.ljust(10))
+print(s.ljust(20))
+'''右对齐'''
+print(s1.rjust(20,'*'))
+print(s1.rjust(20))
+print(s1.rjust(10))
